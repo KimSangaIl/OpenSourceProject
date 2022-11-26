b96f8172acde6f4c740fa00d1dced3a11d302669
TA-PP <kgrewd@naver.com>
2022-11-26 17:20:25 +0900

    Uploading Base Code

---
+++ app/src/main/java/com/example/roommate/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.roommate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText et_id,et_pass,et_name,et_age;
+    private Button btn_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
