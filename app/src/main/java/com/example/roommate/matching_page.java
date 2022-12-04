a45be1f9b4ab997990cf32dbadcec587a8a3bd83
asfff0321 <asfff0321@naver.com>
2022-12-04 20:38:37 +0900

    매칭 시작 페이지 추가

--- app/src/main/java/com/example/roommate/matching_page.java
+++ app/src/main/java/com/example/roommate/matching_page.java
@@ -1,9 +1,14 @@
 package com.example.roommate;
 
+import static androidx.databinding.DataBindingUtil.setContentView;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,10 +20,17 @@ public class matching_page extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_matching_page, container, false);
-    }
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_matching_page, container, false);
+
+        Button matching_start = view.findViewById(R.id.matching_start);
+        matching_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),matching_start_page.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
