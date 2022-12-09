30988c9e82025b5e3da8cf263507e35bb376cb5f
beomjunpark <park97111997@gmail.com>
2022-12-09 17:18:18 +0900

    매칭페이지 다음화면 추가

---
+++ app/src/main/java/com/example/roommate/matching_start2.java
@@ -0,0 +1,17 @@
+package com.example.roommate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class matching_start2 extends AppCompatActivity {
+
+    private Button btn_matching_start;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.matching_page2);
+    }
+}
