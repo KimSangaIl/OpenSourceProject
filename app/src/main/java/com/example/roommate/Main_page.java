1dff5ed0244baf3c9e4d0b34fcd0553af2ef0fe6
asfff0321 <asfff0321@naver.com>
2022-12-10 20:14:51 +0900

    페이지 타이틀 수정 게시판 수정

--- app/src/main/java/com/example/roommate/Main_page.java
+++ app/src/main/java/com/example/roommate/Main_page.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
 public class Main_page extends Fragment {
@@ -14,6 +15,11 @@ public class Main_page extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
+
+        // 타이틀 바 내용 바꾸기
+        ActionBar actionBar = ((MainActivity)getActivity()).getSupportActionBar();
+        actionBar.setTitle("메인 페이지");
+
         return inflater.inflate(R.layout.fragment_main_page, container, false);
     }
     @Override
--- app/src/main/java/com/example/roommate/matching_page.java
+++ app/src/main/java/com/example/roommate/matching_page.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 
 public class matching_page extends Fragment {
@@ -14,6 +16,11 @@ public class matching_page extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // 타이틀 바 내용 바꾸기
+        ActionBar actionBar = ((MainActivity)getActivity()).getSupportActionBar();
+        actionBar.setTitle("룸메이트 매칭");
+
         View view = inflater.inflate(R.layout.fragment_matching_page, container, false);
 
         btn_start = view.findViewById(R.id.matching_start);
